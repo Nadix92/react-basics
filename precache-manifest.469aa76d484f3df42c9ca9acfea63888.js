cb1f377002689945aa8012dfe2d9f3a0d1e54174
Nadix92 <49558241+Nadix92@users.noreply.github.com>
2020-04-22 14:31:39 +0200

    Updates

---
+++ precache-manifest.469aa76d484f3df42c9ca9acfea63888.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c5d080575b3fbca5c6c04168b546711",
+    "url": "/react-basics/index.html"
+  },
+  {
+    "revision": "fb85ab08b134e5e0bfbd",
+    "url": "/react-basics/static/css/main.ff111aae.chunk.css"
+  },
+  {
+    "revision": "fb471c23a73ba7102650",
+    "url": "/react-basics/static/js/2.8dec374b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-basics/static/js/2.8dec374b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb85ab08b134e5e0bfbd",
+    "url": "/react-basics/static/js/main.812c627c.chunk.js"
+  },
+  {
+    "revision": "86a2ff967c94354d0601",
+    "url": "/react-basics/static/js/runtime-main.8590d9a2.js"
+  }
+]);
